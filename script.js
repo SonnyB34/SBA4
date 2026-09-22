f58f463cc19f56f35c7b44bbc7e7eb755e4cd8dc
SonnyB34 <sbboi12@gmail.com>
2026-09-22 11:55:33 -0400

    renderList function

--- script.js
+++ script.js
@@ -3,40 +3,79 @@ let taskCategoryInput = document.getElementById('taskCategory');
 let addBtn = document.getElementById('addTaskBtn');
 let tasksList = document.getElementById('taskList');
 let deadLineInput = document.getElementById('deadLineDate');
+let taskStatus = document.getElementById('taskStatus');
 
-addBtn.addEventListener('click', addTask);
+addBtn.addEventListener('click', function () {
+  let tasks = [];
 
-let tasks = [];
+  let name = inputTask.value;
+  let category = taskCategoryInput.value;
+  let deadline = deadLineInput.value;
+  let status = taskStatus.value;
+  
 
-function addTask() {
+  let newTask = {
+    name: name,
+    category: category,
+    deadline: deadline,
+    status: status,
+  };
+
+
+  console.log(tasks);
+  tasks.push(newTask);
+  renderList();
+  alert('Tasks have been added!');
+  inputTask.value = '';
+  taskCategoryInput.value = '';
+  deadLineInput.value = '';
+  taskStatus.value = 'In Progress';
+
+  function renderList() {
+    tasksList.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+      let listItem = document.createElement('li');
+      let spanName = document.createElement('span');
+      let spanTaskName = document.createElement('span');
+      spanTaskName.id = 'nameSpan';
+      spanTaskName.innerText = `${tasks[i].name}`;
+      spanName.className = 'taskLabel';
+      spanName.innerText = 'Name: '; 
+      spanName.appendChild(spanTaskName);
+      listItem.appendChild(spanName);
+
+     let categorySpan = document.createElement('span');
+     let spanCategory = document.createElement('span');
+     spanCategory.id = 'category-Span';
+     spanCategory.innerText = `${tasks[i].category}`;
+      categorySpan.className = 'taskCat';
+      categorySpan.innerText = 'Category: ';
+      categorySpan.appendChild(spanCategory);
+      listItem.appendChild(categorySpan);
+
+      let deadLineSpan = document.createElement('span');
+      let spanDeadLine = document.createElement('span');
+      spanDeadLine.id = 'span-Deadline';
+      spanDeadLine.innerText = `${tasks[i].deadline}`;
+      deadLineSpan.className = 'taskDue';
+      deadLineSpan.innerText = 'Due: ';
+      deadLineSpan.appendChild(spanDeadLine);
+      listItem.appendChild(deadLineSpan);
+
+      let statusSpan = document.createElement('span');
+      let spanStatus = document.createElement('span');
+      spanStatus.id = 'span-Status';
+      spanStatus.innerText = `${tasks[i].status}`;
+      statusSpan.id = 'statusTask';
+      statusSpan.innerText = 'Status: ';
+      statusSpan.appendChild(spanStatus);
+      listItem.appendChild(statusSpan);
+
+
+      tasksList.appendChild(listItem);
     
-    let name = inputTask.value;
-    let category = taskCategoryInput.value;
-    let deadline = deadLineInput.value;
-
-
-    let newTask = {
-        name: name,
-        category: category,
-        deadline: deadline,
-        status: 'In progress'
-    };
-
-console.log(tasks);
-    tasks.push(newTask);
-    renderList();
-    alert('Tasks have been added!');
-    inputTask.value = '';
-   
-}
-
-
-
-function renderList() {
-  tasksList.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    let listItem = document.createElement('li');
-    listItem.innerText = `NAME: ${tasks[i].name}  CATEGORY: ${tasks[i].category}   DUE: ${tasks[i].deadline}  STATUS: ${tasks[i].status}`;
-    tasksList.appendChild(listItem);
+      
+    }
+    
   }
-}
+});
